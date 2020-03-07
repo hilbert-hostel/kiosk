31a55e84b94cf99b0ed189a3a05be401172896d7
aihuamen <kishisa.yamarashii@gmail.com>
2020-03-07 16:38:14 +0700

    add camera feature?

--- kiosk-gui.py
+++ kiosk-gui.py
@@ -1,19 +1,66 @@
 import pygame
+from picamera import PiCamera
+from time import sleep
+
+red = (200,0,0)
+lightred = (255,0,0)
+green = (0,255,0)
+blue = (0,0,255)
+white = (255,255,255)
+black = (0,0,0)
+X = 1280
+Y= 720
+resolution = (1280,720)
+camera = PiCamera()
 
 pygame.init()
-screen = pygame.display.set_mode((1280,720))
+screen = pygame.display.set_mode((X,Y))
 pygame.display.set_caption("yee")
+
+def text_objects(text, font):
+    textSurface = font.render(text, True, black)
+    return textSurface, textSurface.get_rect()
+
+def button(msg,x,y,w,h,ic,ac,msgz,action=None):
+    mouse = pygame.mouse.get_pos()
+    clicked = pygame.mouse.get_pressed()
+    
+    if(x+w > mouse[0] > x and y+h > mouse[1] > y):
+        pygame.draw.rect(screen, ac, (x,y,w,h))  #posx,posy,dimx,dimy
+        
+        if(clicked[0] == 1 and action != None):
+            action()
+    else:
+        pygame.draw.rect(screen, ic, (x,y,w,h))
+        
+    btnText = pygame.font.Font("freesansbold.ttf",msgz)
+    textSurf, textRect = text_objects(msg, btnText)
+    textRect.center = ( x+w/2, y+h/2 )
+    screen.blit(textSurf, textRect)    
+
+def takePic():
+    camera.start_preview(alpha=192)
+    sleep(3)
+    camera.capture("/home/pi/Desktop/yed1.jpg")
+    camera.stop_preview()
 
 run = True
 
 while run:
-    pygame.time.delay(100) # This will delay the game the given amount of milliseconds. In our casee 0.1 seconds will be the delay
 
     for event in pygame.event.get():  # This will loop through a list of any keyboard or mouse events.
         if event.type == pygame.QUIT: # Checks if the red button in the corner of the window is clicked
             run = False  # Ends the game loop
-
-    pygame.draw.rect(screen, (255,0,0), (100,100,50,50))  
-    pygame.display.update()      
+    
+    screen.fill(white)
+    
+    largeText = pygame.font.SysFont("comicsansms",115)
+    TextSurf, TextRect = text_objects("Hilbert boiz", largeText)
+    TextRect.center = ((X/2),100)
+    screen.blit(TextSurf, TextRect)
+    
+    smileBtn = button("Smile!",X/2-150,Y-200,300,100,red,lightred,50,takePic)
+    
+    pygame.display.update()
 
 pygame.quit()  # If we exit the loop this will execute and close our game
