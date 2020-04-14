846293d0fbbcc74608d301cdafbda67e98190a56
aihuamen <kishisa.yamarashii@gmail.com>
2020-04-14 12:25:44 +0700

    :bug::penguin: wait backend to be fixed

--- api.py
+++ api.py
@@ -6,17 +6,17 @@ h = "https://hilbert.himkwtn.me/checkin"
 
 def gather_info(cr,resv_info):
     cr.read_card()
-    #nw = dt.datetime.now()
-    #nw = nw.strftime("%Y-%m-%d")
-    nw = "2020-04-04"
+    nw = dt.datetime.now()
+    nw = nw.strftime("%Y-%m-%d")
+    #nw = "2020-04-04"
     host = h
     params = {"nationalID":cr.card_data["nationalID"],"date":nw}
     ret = requests.get(host,params)
+    print(ret)
     if ret.status_code == 200:
         temp = dict(ret.json())
         for i in temp:
-            resv_info[i] = temp[i]
-        #print (resv_info)    
+            resv_info[i] = temp[i]    
         load_pic(temp['rooms'][0]['photos'][0]['photo_url'])
         resize_pic("room_image.jpg",350,"resized_room")
         resize_pic("room_image.jpg",250,"resized_room2")
@@ -24,7 +24,7 @@ def gather_info(cr,resv_info):
 def request_OTP(resv_info,refn):
     host = (h+"/generate-otp/{}").format(resv_info["id"])
     ret = requests.post(host)
-    print(ret.request.headers)
+    print(ret)
     if ret.status_code == 200:
         temp = dict(ret.json())
         for i in temp:
@@ -34,20 +34,21 @@ def request_OTP(resv_info,refn):
     
 def verify_OTP(resv_info,otp,token):
     host = h+"/verify-otp/{}".format(resv_info["id"])
+    print(host)
     notp = ""
     for i in otp:
         notp += str(i)    
     body = {'otp':notp}
     ret = requests.post(host,json = body)
+    print(ret)
     if ret.status_code == 200:
         temp = dict(ret.json())
         for i in temp:
             token[i] = temp[i]
-    print(token)
 
 def send_data(cr,token):
-    print(cr.card_data)
     host = h
-    hdr = {'Authentication':'Bearer '+token['token']}
+    hdr = {'Authorization':'Bearer '+token['token']}
     ret = requests.post(host,files = cr.card_data,headers=hdr)
     print(ret)
+    print(dict(ret.json()))
